daf072b4893f6f72ef74d56bbdf8903cd483a250
Binh Le Van <120247055+binhlevan48@users.noreply.github.com>
2023-06-17 15:30:15 +0700

    Update Bigquery_project.sql

--- SQL/Bigquery_Project/Bigquery_project.sql
+++ SQL/Bigquery_Project/Bigquery_project.sql
@@ -1,5 +1,8 @@
 -- Big project for SQL
 -- Link instruction: https://docs.google.com/spreadsheets/d/1WnBJsZXj_4FDi2DyfLH1jkWtfTridO2icWbWCh7PLs8/edit#gid=0
+-- In this project, we will write 08 query in Bigquery base on Google Analytics dataset.
+-- Table Schema: https://support.google.com/analytics/answer/3437719?hl=en
+-- Format Element: https://cloud.google.com/bigquery/docs/reference/standard-sql/format-elements
 
 
 
